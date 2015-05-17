be40c2163094a123bab2d2bc9abf57d2e12c526d
Buddy Patton <buddyp450@gmail.com>
2015-05-17 10:32:23 -0400

    Materials, prefabs, scripts, and scenes currently being used.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 1.0f;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	// Called just before performing any physics calculations
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis("Horizontal"); // left and right
+		float moveVertical = Input.GetAxis("Vertical"); // up and down
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		//Destroy(other.gameObject);
+		if(other.gameObject.CompareTag ("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+		if(count >= 18)
+		{
+			winText.text = "You Win";
+		}
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Rotates the object constantly over time
+ **/
+public class Rotator : MonoBehaviour {
+
+	public float rotationSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime * rotationSpeed);
+	}
+}
